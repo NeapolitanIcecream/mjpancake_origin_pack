afbbbc304b54a10fed53ae21ea914f21ad38d776
Neopolitan <neopolitan@mail.ustc.edu.cn>
2019-02-19 14:34:52 +0800

    fix #2

--- Koromo.girl.lua
+++ Koromo.girl.lua
@@ -35,11 +35,112 @@ function copy(obj)
     return res
 end
 
+-- 判斷筋牌（有序）
+function isJin(t1, t2)
+    return t1:isnum() and t2:isnum() and t1:suit() == t2:suit() and t1:val() + 3 == t2:val()
+end
+
+-- 判斷可以明槓
+function canMinKan(closed, t)
+    return closed:ct(t) == 3
+end
+
+-- 判斷可以碰
+function canPon(closed, t)
+    return closed:ct(t) >= 2
+end
+
+-- 判斷可以吃
+function canChii(closed, t)
+    return canChiiAsLeft(closed, t) or canChiiAsMiddle(closed, t) or canChiiAsRight(closed, t)
+end
+
+-- 左副露吃
+function canChiiAsLeft(closed, t)
+    if (t:isz() or t:val() >= 8) then
+        return false
+    end
+
+    -- 這裡用 dora hack next
+    local ccal_n = t:dora()
+    local ccal_nn = ccal_n:dora()
+    if (not (closed:ct(ccal_n) > 0 and closed:ct(ccal_nn) > 0)) then
+        return false
+    end
+
+    -- 計算食替
+    for _, ccal_t in ipairs(T34.all) do
+        local ccal_ct = closed:ct(ccal_t)
+        if
+            ccal_ct >= 1 and ((not (ccal_t == ccal_n) and not (ccal_t == ccal_nn)) or ccal_ct >= 2) and
+                not (ccal_t == t) and
+                not (isJin(t, ccal_t))
+         then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- 中副露吃
+function canChiiAsMiddle(closed, t)
+    if (t:isz() or t:val() == 1 or t:val() == 9) then
+        return false
+    end
+
+    -- 這裡用 indicator hack prev
+    local ccal_n = t:dora()
+    local ccal_p = t:indicator()
+    if (not (closed:ct(ccal_n) > 0 and closed:ct(ccal_p) > 0)) then
+        return false
+    end
+
+    -- 計算食替
+    for _, ccal_t in ipairs(T34.all) do
+        local ccal_ct = closed:ct(ccal_t)
+        if ccal_ct >= 1 and ((not (ccal_t == ccal_n) and not (ccal_t == ccal_p)) or ccal_ct >= 2) and not (ccal_t == t) then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- 右副露吃
+function canChiiAsRight(closed, t)
+    if (t:isz() or t:val() <= 2) then
+        return false
+    end
+
+    local ccal_p = t:indicator()
+    local ccal_pp = ccal_p:indicator()
+    if (not (closed:ct(ccal_p) > 0 and closed:ct(ccal_pp) > 0)) then
+        return false
+    end
+
+    -- 計算食替
+    for _, ccal_t in ipairs(T34.all) do
+        local ccal_ct = closed:ct(ccal_t)
+        if
+            ccal_ct >= 1 and ((not (ccal_t == ccal_p) and not (ccal_t == ccal_pp)) or ccal_ct >= 2) and
+                not (ccal_t == t) and
+                not (isJin(ccal_t, t))
+         then
+            return true
+        end
+    end
+
+    return false
+end
+
 -- 每局开始时初始化全局变量
 function checkinit()
     if who ~= self then
         return true
     end
+
+    ready = false
 
     -- 处理天听海底
     if init:ready() then
@@ -50,6 +151,7 @@ function checkinit()
             if mount:remaina(t) >= 1 then
                 mount:loadb(t, 1)
                 succ = true
+                ready = true
                 undersea = t
                 wait = copy(listen) -- 记录听牌
                 print(t:str34() .. " 已经沉入海底")
@@ -68,51 +170,70 @@ function ondraw()
     remain = mount:remainpii()
     local hand = game:gethand(who)
 
+    -- 计算海底方位
+    local mod = remain % 4
+    local time = math.floor(remain / 4)
+
     -- 地狱一向听
-    if who ~= self and (not (rinshan)) and (hand:step() == 1 or hand:step7() == 1 or hand:step13() == 1) then
+    if who ~= self and (not (rinshan)) and hand:step() == 1 then
         for _, t in ipairs(hand:effa()) do
             local n = mount:remaina(t)
             mount:lighta(t, (n * (-10)) * rate_other)
         end
     end
 
-    --[[
-    -- 提高鸣牌和直击机会，压制他家鸣牌
-    if who ~= self then
-        local hand_slf = game:gethand(self)
-        local hand_left = game:gethand(who:left())
-        local hand_cross = game:gethand(who:cross())
-        local hand_right = game:gethand(who:right())
-
-        if not (hand_slf:ready()) then
-            for _, t in ipairs(hand_slf:effa()) do
-                local n = mount:remaina(t)
-                mount:lighta(t, n * 20 * rate_other)
-            end
+    -- 非海底路线提高直击机会
+    if who ~= self and game:gethand(self):ready() and not (ready) then
+        for _, t in ipairs(game:gethand(self):effa()) do
+            local n = mount:remaina(t)
+            mount:lighta(t, (n * 10) * rate_other)
         end
+    end
 
-        if not (hand_left:ready()) then
-            for _, t in ipairs(hand_left:effa()) do
-                local n = mount:remaina(t)
-                mount:lighta(t, n * (-10) * rate_other)
+    -- 提高鸣牌机会
+    if who == self:left() and (not (rinshan)) and mod == 1 and not (ready) then
+        closed = game:gethand(self):closed()
+        it_hand = hand
+        for _, t in ipairs(T34.all) do
+            if canChii(closed, t) then
+                mount:lighta(t, 125 * rate_other)
             end
-        end
-
-        if not (hand_cross:ready()) then
-            for _, t in ipairs(hand_cross:effa()) do
-                local n = mount:remaina(t)
-                mount:lighta(t, n * (-10) * rate_other)
-            end
-        end
-
-        if not (hand_right:ready()) then
-            for _, t in ipairs(hand_right:effa()) do
-                local n = mount:remaina(t)
-                mount:lighta(t, n * (-10) * rate_other)
+            for _, tt in ipairs(it_hand:effa()) do
+                if tt == t then
+                    mount:lighta(t, -125 * rate_other)
+                    break
+                end
             end
         end
     end
-    ]]
+
+    if who ~= self and (not (rinshan)) and mod == 1 and not (ready) then
+        closed = game:gethand(self):closed()
+        it_hand = hand
+        for _, t in ipairs(T34.all) do
+            if canPon(closed, t) then
+                mount:lighta(t, 125 * rate_other)
+            end
+            for _, tt in ipairs(it_hand:effa()) do
+                if tt == t then
+                    mount:lighta(t, -125 * rate_other)
+                    break
+                end
+            end
+        end
+    end
+
+    -- 準備海底後，壓制他家鳴牌
+    if who ~= self and (not (rinshan)) and ready then
+        closedr = game:gethand(who:right()):closed()
+        closedl = game:gethand(who:left()):closed()
+        closedc = game:gethand(who:cross()):closed()
+        for _, t in ipairs(T34.all) do
+            if canChii(closedr, t) or canPon(closedr, t) or canPon(closedl, t) or canPon(closedc, t) then
+                mount:lighta(t, -125 * rate_other)
+            end
+        end
+    end
 
     -- 准备海底捞月
     if who == self and not (compare(hand:effa(), wait)) and hand:ready() then
@@ -122,6 +243,7 @@ function ondraw()
             if mount:remaina(t) >= 1 then
                 mount:loadb(t, 1)
                 succ = true
+                ready = true
                 undersea = t
                 wait = copy(listen)
                 print(t:str34() .. " 已经沉入海底")
@@ -129,12 +251,13 @@ function ondraw()
             end
         end
         if not (succ) then
+            ready = false
             print("海底残枚不足")
         end
     end
 
     if who == self then
-        -- todo: 早巡大牌直击
+        -- 加速听牌
         if not (hand:ready()) then
             for _, t in ipairs(hand:effa()) do
                 local n = mount:remaina(t)
@@ -142,9 +265,6 @@ function ondraw()
             end
         end
 
-        -- 计算海底方位
-        local mod = remain % 4
-        local time = math.floor(remain / 4)
         if (mod == 1) then
             print(time .. "巡后，自家到达海底")
         elseif (mod == 2) then
