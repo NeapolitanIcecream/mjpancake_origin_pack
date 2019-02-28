e0fc0b5e452c7b70d7b156fd7879829e30425a6f
Neopolitan <neopolitan@mail.ustc.edu.cn>
2019-02-28 21:53:02 +0800

    update aislinn, add saki

--- Aislinn.girl.lua
+++ Aislinn.girl.lua
@@ -1,73 +1,154 @@
 rate = 1
-incmk = 1000
+incmk = 100000
 
 -- 线性同余随机数发生器
 a = 1103515245
 c = 12345
-m = 2 ^ 32
+m = 2 ^ 32 - 1
+seed = 1
 
-function random(seed)
-    return (a * seed + c) % m
+function random()
+    seed = (a * seed + c) % m
+    return seed
 end
 
--- 生成理想画面
 function checkinit()
-    if who ~= self then
+    if who ~= self or iter > 100 then
         return true
     end
 
-    local hand = init
-    local effas = hand:effa()
-    local steps = math.min(hand:step(), hand:step7(), hand:step13())
-    local seed = game:getround() + game:getextraround() -- 种子
-    local len = #effas -- 序列长度
-    local j = math.floor(random(seed)) % len + 1 -- 骰子
-
-    loc = self:turnfrom(game:getdealer()) -- 庄家距离定位
-    turn = 0 -- 回合统计
-    paint = {} -- 约定进张
-    count = {} -- 牌数统计
-
-    -- 统计手牌数目
-    for _, t in ipairs(T34.all) do
-        count[t:id34()] = hand:ct(t)
-    end
-
-    for i = 1, 13 do
-        local k = 1 -- 骰子计数
-        local loop = false -- 有效循环
-
-        repeat
-            -- 从头循环
-            if k > len then
-                k = 1
-                loop = true
-            end
-
-            -- 记录进张
-            local t = effas[k]
-            if k >= j or loop then
-                if count[t:id34()] < 4 then
-                    paint[i] = t
-                    count[t:id34()] = count[t:id34()] + 1
-                    print(i .. "巡进张为" .. t:str34())
-                    break
-                end
-            end
-
-            k = k + 1
-        until k > len and loop
-
-        j = math.floor(random(j)) % len + 1
-    end
-
-    return true
+    return init:step() <= 4
 end
 
 function ondraw()
-    if who:turnfrom(game:getdealer()) == loc then
+    if round_started then
+        local hand = {}
+        hand[0] = game:gethand(self)
+        hand[1] = game:gethand(self:right())
+        hand[2] = game:gethand(self:cross())
+        hand[3] = game:gethand(self:left())
+        local effas = hand[0]:effa()
+        local len = #effas -- 序列长度
+        local j = random() % len + 1 -- 骰子
+
+        drawn = (-1) * self:turnfrom(game:getdealer()) -- 抽牌统计
+        turn = 0 -- 回合統計
+        cntturn = 0 -- turnmax 统计
+        cntpaint = {} -- cnt[paint[turn]]
+        paint = {} -- 约定进张
+        count = {} -- 牌数统计
+        loadb = false -- have been loaded to mountb
+        is_kan = false -- avoid kan change order
+        round_started = false
+
+        -- 初始化 count，cntpaint
+        for _, t in ipairs(T34.all) do
+            count[t:id34()] = 0
+            cntpaint[t:id34()] = 0
+        end
+
+        for _, t in ipairs(T34.all) do
+            for i = 0, 3 do
+                count[t:id34()] = count[t:id34()] + hand[i]:ct(t)
+            end
+        end
+
+        for i = 1, 13 do
+            local k = 1 -- 骰子计数
+            local loop = false -- 有效循环
+            local flag = true
+
+            repeat
+                -- 从头循环
+                if k > len then
+                    k = 1
+                    loop = true
+                end
+
+                -- 记录进张
+                local t = effas[k]
+                if k >= j or loop then
+                    if count[t:id34()] < 4 then
+                        paint[i] = t:id34()
+                        count[t:id34()] = count[t:id34()] + 1
+                        cntturn = cntturn + 1
+                        cntpaint[t:id34()] = cntpaint[t:id34()] + 1
+                        flag = false
+                        print(i .. "巡进张为" .. t:str34()) -- .. " debug " .. cntturn .. " " .. count[t:id34()])
+                        break
+                    end
+                end
+
+                k = k + 1
+            until k > len and loop
+
+            if flag then
+                break
+            end
+
+            j = random() % len + 1
+        end
+
+        -- loaded to b
+        for i = 1, cntturn do
+            mount:loadb(T34.new(paint[i]), cntpaint[paint[i]])
+        end
+    end
+
+    if is_kan then
+        return
+    end
+
+    if drawn == 0 and turn < cntturn then
+        drawn = drawn - 4
         turn = turn + 1
-        mount:lighta(paint[turn], incmk)
+        local t = T34.new(paint[turn])
+        local tt = 0
+        local dis = who:turnfrom(self)
+
+        mount:lightb(t, incmk * rate)
+
+        if dis == 0 then
+            print("进张位置为自家")
+        elseif dis == 1 then
+            print("进张位置为下家")
+        elseif dis == 2 then
+            print("进张位置为对家")
+        elseif dis == 3 then
+            print("进张位置为上家")
+        end
+
+        if turn < cntturn then
+            tt = T34.new(paint[turn + 1])
+            print("本巡进张预计为" .. t:str34() .. "，下一巡进张预计为" .. tt:str34())
+        else
+            print("本巡进张预计为" .. t:str34())
+        end
     end
 end
 
+function ongameevent()
+    -- round-started
+    if event.type == "round-started" then
+        round_started = true
+    end
+
+    -- drawn
+    if event.type == "drawn" then
+        if not (is_kan) then
+            drawn = drawn + 1
+        else
+            is_kan = false
+            -- print("debug-deal-kan")
+        end
+    end
+
+    -- barked
+    if event.type == "barked" then
+        local bark = event.args["bark"]
+        if bark:type() == "daiminkan" or bark:type() == "ankan" or bark:type() == "kakan" then
+            is_kan = true
+            -- print("debug-found-kan")
+        end
+    end
+end
---
+++ Saki.girl.lua
@@ -0,0 +1,102 @@
+rate_self = 1
+rate_others = 1
+incmk_rins = 1000
+incmk_3f = 40
+incmk_pg = {[0] = 0, [1] = 0, [2] = 80, [3] = 160, [4] = 0}
+incmk_dkg = {[0] = 0, [1] = 40, [2] = 80, [3] = 160, [4] = 0}
+incmk_kakan = 1000
+
+-- 增加西風配牌
+function onmonkey()
+    local exist = exists[self:index()]
+    exist:incmk(T34.new("3f"), incmk_3f * rate_self)
+end
+
+function checkinit()
+    if who ~= self or iter > 500 then
+        return init:ct(T34.new("3f")) == 0
+    end
+
+    if init:ct(T34.new("3f")) >= 1 and init:step7() <= 3 then
+        koncai = {}
+        cnt_koncai = 0
+        return true
+    end
+
+    return false
+end
+
+function ondraw()
+    local hand_self = game:gethand(self)
+    local closed_self = hand_self:closed()
+    local bark_self = hand_self:barks()
+    local effa_self = hand_self:effa()
+    local hand_other = game:gethand(who)
+    local effa_other = hand_other:effa()
+    local ready_self = hand_self:ready()
+
+    -- 沈底西風，加槓槓材
+    if who ~= self then
+        mount:lighta(T34.new("3f"), (-1) * incmk_3f * rate_others)
+        for i = 1, cnt_koncai do
+            mount:lighta(koncai[i], (-1) * incmk_kakan * rate_others)
+        end
+    end
+
+    -- 容易碰/明槓
+    if who ~= self and not (ready) then
+        for _, t in ipairs(T34.all) do
+            local flag = true
+
+            for _, tt in ipairs(effa_other) do
+                if t:id34() == tt:id34() then
+                    flag = false
+                    break
+                end
+            end
+
+            if flag then
+                -- print(t:str34() .. " pg " .. incmk_pg[closed_self:ct(t)] * rate_others)
+                mount:lighta(t, incmk_pg[closed_self:ct(t)] * rate_others)
+            else
+                mount:lighta(t, (-1) * incmk_pg[closed_self:ct(t)] * rate_others)
+            end
+        end
+    end
+
+    if who == self then
+        -- 容易獲得對/刻/暗槓/西風
+        for _, t in ipairs(T34.all) do
+            -- print(t:str34() .. " dkg" .. incmk_dkg[closed_self:ct(t)] * rate_self)
+            mount:lighta(t, incmk_dkg[closed_self:ct(t)] * rate_self)
+        end
+
+        mount:lighta(T34.new("3f"), incmk_3f * rate_self)
+
+        -- 容易加槓
+        for _, m in ipairs(bark_self) do
+            if m:type() == "pon" then
+                -- print(m[1]:str34() .. " kakan " .. incmk_kakan * rate_self)
+                mount:lighta(m[1], incmk_kakan * rate_self)
+            end
+        end
+
+        -- 從嶺上獲得有效牌
+        if rinshan then
+            for _, t in ipairs(effa_self) do
+                -- print(t:str34() .. " rins " .. incmk_rins * rate_self)
+                mount:lighta(t, incmk_rins * rate_self)
+            end
+        end
+    end
+end
+
+function ongameevent()
+    if event.type == "barked" and event.args["who"] == self and event.args["bark"]:type() == "pon" then
+        local bark = event.args["bark"]
+        cnt_koncai = cnt_koncai + 1
+        koncai[cnt_koncai] = bark[1]
+    end
+end
+
+-- todo：正負零，開槓不加寶牌
